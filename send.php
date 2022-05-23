316c9862e50f37693eaf8c358ab365758ec2ff73
Astellono <94223615+Astellono@users.noreply.github.com>
2022-05-23 12:01:41 +0300

    php

---
+++ send.php
@@ -0,0 +1,17 @@
+<?php
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+
+$fio = urldecode($fio);
+$email = urldecode($email);
+
+$fio = trim($fio);
+$email = trim($email);
+
+echo $fio;
+echo "<br>";
+echo $email;
+?>
