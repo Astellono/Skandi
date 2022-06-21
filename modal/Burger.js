9d5e40662828c5c55cdd45b2a323b301129ff506
Astellono <94223615+Astellono@users.noreply.github.com>
2022-06-21 17:04:49 +0300

    n

--- modal/Burger.js
+++ modal/Burger.js
@@ -1,8 +1,7 @@
-
 document.querySelector(".header__burger").addEventListener("click", function() {
-  document.querySelector(".header__menu").classList.add("header__burger-open");
+    document.querySelector(".header__menu").classList.add("header__burger-open");
 })
 
 document.querySelector(".header__burger-cross").addEventListener("click", function() {
-  document.querySelector(".header__menu").classList.remove("header__burger-open");
+    document.querySelector(".header__menu").classList.remove("header__burger-open");
 })
