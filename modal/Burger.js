67753fd9c5bbce195e01d9e1e7681b834fe34f76
Astellono <94223615+Astellono@users.noreply.github.com>
2022-07-26 11:51:31 +0300

    new

--- modal/Burger.js
+++ modal/Burger.js
@@ -5,3 +5,7 @@ document.querySelector(".header__burger").addEventListener("click", function() {
 document.querySelector(".header__burger-cross").addEventListener("click", function() {
     document.querySelector(".header__menu").classList.remove("header__burger-open");
 })
+
+if (document.location.href.indexOf('showModal') != -1) {
+    $("#modal-kirgiziya").modal('show');
+  }
