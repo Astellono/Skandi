472721a15dc58cbaef30d3eaf9e23eb4a109ce76
Astellono <94223615+Astellono@users.noreply.github.com>
2022-09-09 16:38:49 +0300

    лгпргп

--- modal/Burger.js
+++ modal/Burger.js
@@ -1,11 +1,34 @@
-document.querySelector(".header__burger").addEventListener("click", function() {
+document.querySelector(".header__burger").addEventListener("click", function () {
     document.querySelector(".header__menu").classList.add("header__burger-open");
 })
 
-document.querySelector(".header__burger-cross").addEventListener("click", function() {
+document.querySelector(".header__burger-cross").addEventListener("click", function () {
     document.querySelector(".header__menu").classList.remove("header__burger-open");
 })
 
 if (document.location.href.indexOf('showModal') != -1) {
     $("#modal-kirgiziya").modal('show');
-  }
+}
+
+
+function sumVector(vec1, vec2) {
+    let finalVec = []
+
+    while (vec1.length != vec2.length) {
+        let tmp = vec1.length - vec2.length
+
+        if (tmp > 0) {
+            vec2.push(0)
+        } else vec1.push(0)
+    }
+    for (let i = 0; i < vec1.length; i++) {
+        console.log(vec2);
+        finalVec.push(vec1[i] + vec2[i])
+    }
+
+    return finalVec
+}
+
+vector1 = [1, 1, 1, 2, 5]
+vector2 = [2, 2, 2]
+console.log(sumVector(vector1, vector2));
