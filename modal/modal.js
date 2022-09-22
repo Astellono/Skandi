567ce2b444519e5c47a4c7ba96745ed89719224f
Astellono <94223615+Astellono@users.noreply.github.com>
2022-09-22 16:42:31 +0300

    finish

--- modal/modal.js
+++
@@ -1,6 +0,0 @@
-// var myModal = document.getElementById('myModal')
-// var myInput = document.getElementById('myInput')
-// var myModal = new bootstrap.Modal(document.getElementById('myModal'), options)
-// myModal.addEventListener('shown.bs.modal', function () {
-//   myInput.focus()
-// })
