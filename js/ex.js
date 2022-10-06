099d205c4c5283d460e9868a2c89f68333b563b6
Astellono <94223615+Astellono@users.noreply.github.com>
2022-10-06 12:26:21 +0300

    ВВ

--- js/ex.js
+++ js/ex.js
@@ -1,12 +1,26 @@
 var elements = document.querySelectorAll(".excursions__item");
-var elArrow = document.querySelectorAll('.excursions__arrrow');
+var elLink = document.querySelectorAll('.excursions__link');
 var elHidden = document.querySelectorAll('.excursions__item-desc-hidden');
 for (var i = 0; i < elements.length; i++) {
     console.log(i);
-    elements[i].onclick = function () {
+    elements[i].onclick = function (event) {
         console.log(i);
+        event.stopPropagation()
         this.classList.toggle('long');
 
 
     };
 }
+// for (var i = 0; i < elLink.length; i++) {
+//     console.log(i);
+//     elLink[i].onclick = function () {
+        
+        
+//     };
+// }
+// function stopStyle() {
+//     console.log();
+//     let el = document.querySelectorAll(".excursions__item");
+//     console.log(el[index].classList);
+   
+// }
