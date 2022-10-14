2b418fbd0c02ab11b128bb02863bc5e872b54127
Astellono <94223615+Astellono@users.noreply.github.com>
2022-10-14 16:20:05 +0300

    new

--- php/tour/pskov.php
+++ php/tour/pskov.php
@@ -46,7 +46,7 @@ Email: $email
  
 /* А эта функция как раз занимается отправкой письма на указанный вами email */
 $sub="Анкета $fio"; //сабж
-$email="Армения"; // от кого
+$email="Псков"; // от кого
 $send = mail ($myaddres,$sub,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$email <pomiruspalkami@website.ru>");
  
 ini_set('short_open_tag', 'On');
