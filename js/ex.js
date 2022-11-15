61a1c5b4ba893c7bbc2e5cb9488c251175526d5a
Astellono <94223615+Astellono@users.noreply.github.com>
2022-11-15 13:20:07 +0300

    hash

--- js/ex.js
+++ js/ex.js
@@ -1,36 +1,83 @@
 let elements = document.querySelectorAll(".excursions__item");
 let podr = document.querySelectorAll(".exursions__podr");
-elements.forEach(element => {
-    element.onclick = function (event) {
-        element.classList.toggle('long');
-        // window.location.hash='example';
-    };
-    
-});
-    
-
-    
+let h1 = document.querySelector('.excursions__title')
+function render() {
 
 
+    elements.forEach(element => {
+
+        if (window.location.hash === '#' + element.id) {
+            element.style.marginTop = '80px'
+            element.classList.add('long');
+            createBtnBack(element)
+        } else {
+            element.style.display = 'none'
+            h1.style.display = 'none'
+        }
+
+        if (window.location.hash === '') {
+            element.classList.remove('long');
+            element.style.display = 'flex'
+            h1.style.display = 'block'
+            element.style.marginTop = '0px'
+            let btnMass = document.querySelectorAll('.btn-back')
+            btnMass.forEach((e) => {
+                e.remove()
+            })
+        }
+
+        element.onclick = function () {
+            let flag = 0;
+
+            element.classList.add('long');
+            window.location.hash = element.id
+
+            if (window.location.hash != '#' + element.id) {
+                window.location.hash = element.id;
+
+            }
+
+            elements.forEach(element => {
+                if ('#' + element.id != window.location.hash) {
+                    element.style.display = 'none'
+                    h1.style.display = 'none'
+
+                } else {
+                    element.style.display = 'flex'
+                    element.style.marginTop = '80px'
+                }
+            })
+            console.log(flag);
+
+            createBtnBack(element)
+            flag = 1
 
 
 
+        };
 
 
 
-// function toggleDisplay() {
+    })
+}
+render()
+// elements.forEach(element => {
+//     console.log(element.id)
+window.onhashchange = function () {
+    render()
+}
 
-//     if (this.parentNode.classList.contains('current-item')) {
+// });
+function createBtnBack(el) {
+    let btnBack = document.createElement('img')
+    btnBack.setAttribute('src', 'img/arrow.svg')
+    btnBack.classList.add('btn-back')
+    btnBack.addEventListener('click', (e) => {
+        e.stopPropagation()
+        window.location.hash = ''
+        btnBack.remove()
+        render()
+    })
+    el.append(btnBack)
 
-//         var currentlyDisplayed = document.querySelectorAll('.current-item');
-
-//         for (var e=0; e<currentlyDisplayed.length; e++) {
-
-//             currentlyDisplayed[e].classList.remove('current-item');
-//         }
-
-//     } else {
-
-//         this.closest('.item').classList.add('current-item');
-//     }
-// }
+}
