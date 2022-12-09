3926a447f316167b1017b185d57a47f40921a5e8
Astellono <94223615+Astellono@users.noreply.github.com>
2022-12-09 15:33:12 +0300

    new

--- modal/modal.js
+++ modal/modal.js
@@ -27,3 +27,10 @@ for (var i = 0; i < btn_map.length; i++) {
         document.body.style.overflow = 'hidden';
     };
 }
+
+let heightBox = document.querySelector('.tour__page__infoBlock').clientHeight
+let heightIMG = document.querySelector('.tour__page__imgBox')
+let str = String(heightBox)
+console.log(String(heightBox));
+heightIMG.style.height = String(heightBox) +'px' 
+console.log(heightIMG.clientHeight);
