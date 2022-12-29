ae71f19b567cd80a8d7756664289e03764cf34d9
Astellono <94223615+Astellono@users.noreply.github.com>
2022-12-29 20:14:45 +0300

    newYear

--- php/excursions/tea.php
+++ php/excursions/tea.php
@@ -25,7 +25,7 @@ Email: $email
  
 /* А эта функция как раз занимается отправкой письма на указанный вами email */
 $sub="Анкета $fio"; //сабж
-$email="Новогодняя Москва"; // от кого
+$email="Чаепитие"; // от кого
 $send = mail ($myaddres,$sub,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$email <pomiruspalkami@website.ru>");
  
 ini_set('short_open_tag', 'On');
