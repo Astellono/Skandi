3e25672ea157c2ecb75ac0c27a64f5e6d4f4d91f
Astellono <94223615+Astellono@users.noreply.github.com>
2023-04-03 13:44:02 +0300

    ver css

--- modal/Burger.js
+++ modal/Burger.js
@@ -1,3 +1,32 @@
+let cssId = 'myCss';  // you could encode the css path itself to generate id..
+let cssId2 = 'myCssAdd';
+let ver = '505'
+if (!document.getElementById(cssId)) {
+    var head = document.getElementsByTagName('head')[0];
+    var link = document.createElement('link');
+    link.id = cssId;
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = '/style/style.css?ver=' + ver;
+    link.media = 'all';
+    head.appendChild(link);
+}
+if (!document.getElementById(cssId2)) {
+    var head = document.getElementsByTagName('head')[0];
+    var link = document.createElement('link');
+    link.id = cssId;
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = '/style/style-adaptive.css?ver=' + ver;
+    link.media = 'all';
+    head.appendChild(link);
+}
+
+
+
+
+
+
 document.querySelector(".header__burger").addEventListener("click", function () {
     document.querySelector(".header__menu").classList.add("header__burger-open");
 })
@@ -11,14 +40,14 @@ if (document.location.href.indexOf('showModal') != -1) {
 }
 
 let div = document.getElementById('openModal')
-div.addEventListener('click', ()=> {
+div.addEventListener('click', () => {
     document.body.style.overflowY = 'visible'
 })
 
 
-let btn = document.querySelector('.tour__page__btn') 
+let btn = document.querySelector('.tour__page__btn')
 
-btn.addEventListener('click', ()=>{
+btn.addEventListener('click', () => {
     document.body.style.overflowY = 'hidden'
 })
 
