94b3e0e12ac148fab3bfc7f51450125b9a3ec851
Astellono <94223615+Astellono@users.noreply.github.com>
2023-04-03 17:05:15 +0300

    ver css 1 click

---
+++ modal/verCss.js
@@ -0,0 +1,23 @@
+let cssId = 'myCss';  // you could encode the css path itself to generate id..
+let cssId2 = 'myCssAdd';
+let ver = '505'
+if (!document.getElementById(cssId)) {
+    var head = document.getElementsByTagName('head')[0];
+    var link = document.createElement('link');
+    link.id = cssId;
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = '/style/style.css?ver=' + ver;
+    link.media = 'all';
+    head.appendChild(link);
+}
+if (!document.getElementById(cssId2)) {
+    var head = document.getElementsByTagName('head')[0];
+    var link = document.createElement('link');
+    link.id = cssId;
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = '/style/style-adaptive.css?ver=' + ver;
+    link.media = 'all';
+    head.appendChild(link);
+}
