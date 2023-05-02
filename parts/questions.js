b63cd373cb9a3d2247847860c084dc26b0935501
Astellono <94223615+Astellono@users.noreply.github.com>
2023-05-02 12:55:17 +0300

    new form js

--- parts/questions.js
+++ parts/questions.js
@@ -1,7 +1,7 @@
 document.getElementById("questions").innerHTML = '\
 <div class="container">\
 <h2 class="questions__title">Задать вопрос</h2>\
-<form action="php/send.php" method="POST" class="questions__form">\
+<form action="/php/send.php" method="POST" class="questions__form">\
     <input type="text" name="name" placeholder="Ваше имя" required>\
     <input type="text" name="email" placeholder="email" required>\
     <input required type="text" class="questions__input-text" name="message" placeholder="Ваш вопрос?">\
