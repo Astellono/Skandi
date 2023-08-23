47365bb13992723375e60f62b79d9cc0159ade4c
Astellono <94223615+Astellono@users.noreply.github.com>
2023-08-23 11:18:35 +0300

    fix contact

---
+++ parts/contact.js
@@ -0,0 +1,15 @@
+document.getElementById("contacts").innerHTML = '\
+<div class="container">\
+            <h2 class="contacts__title">\
+                Контакты\
+            </h2>\
+            <ul class="contacts__list">\
+                <li class="contacts__item">Телефон:\
+                    <tel>+7 (916) 202-73-90</tel>\
+                </li>\
+                <li class="contacts__item">Телеграмм: <a class="contact__t-link"\
+                        href="https://t.me/+gH_EZn6OVsczNTJi">https://t.me/+gH_EZn6OVsczNTJi</a></li>\
+                <li class="contacts__item">VK: <a class="contact__t-link"\
+                        href="https://vk.com/pomiru_spalkami">vk.com/pomiru_spalkami</a></li>\
+            </ul>\
+        </div>'
