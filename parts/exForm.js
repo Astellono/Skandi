64b5f9266759333b05dc2fc808785ab3f78671e6
Astellono <94223615+Astellono@users.noreply.github.com>
2023-09-18 17:04:54 +0300

    т

--- parts/exForm.js
+++ parts/exForm.js
@@ -1,8 +1,21 @@
 let linkForm = document.querySelectorAll('.excursions__link');
 let exForm = document.getElementById('exForm')
+let modal = document.querySelector('.mod')
 
 linkForm.forEach(e => {
-    e.addEventListener('click', () => {     
+    let nameHash = '#form' + e.dataset.name
+    e.addEventListener('click', () => {
+
+        let nameHash = '#form' + e.dataset.name
+        // console.log(window.location.hash);
+        modal.id = nameHash.slice(1)
+        e.setAttribute('href', nameHash )
+        
+        
         exForm.setAttribute('action', 'php/excursion/' + e.dataset.name +'.php')
+
+
     })
 });
+exForm.setAttribute('action', 'php/excursion/' + window.location.hash.slice(5) +'.php')
+modal.id = window.location.hash.slice(1)
