346c5d0a9fbfa1cc12611fe632bd2f41250235d1
Astellono <94223615+Astellono@users.noreply.github.com>
2024-03-04 17:12:49 +0300

    fix late

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let textAbout = document.getElementById('textAbout')
+let textBot = document.getElementById('storyBot')
+let cnt =0;
+textAbout.addEventListener('click',()=> {
+    cnt++;
+    textAbout.classList.toggle("about__storyFull")
+    if (cnt % 2 === 0) {
+        textBot.textContent=""
+        textBot.innerHTML = "<p>Подробнее<br>▼</p>"
+        console.log("Второй клик");
+    } else textBot.innerHTML = "<p>▲</p>"
+})
+textBot.addEventListener('click',()=> {
+    cnt++;
+    textAbout.classList.toggle("about__storyFull")
+    if (cnt % 2 === 0) {
+        textBot.textContent=""
+        textBot.innerHTML = "<p>Подробнее<br>▼</p>"
+        console.log("Второй клик");
+    } else textBot.innerHTML = "<p>▲</p>"
+})
