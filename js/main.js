8ffd2f2b42a0d46ffee545eb134605873913b3d1
Astellono <94223615+Astellono@users.noreply.github.com>
2024-07-13 12:00:56 +0300

    new

--- js/main.js
+++ js/main.js
@@ -2,24 +2,17 @@ let textAbout = document.getElementById('textAbout')
 let textBot = document.getElementById('storyBot')
 let cnt = 0;
 
-
-textAbout.addEventListener('click',()=> {
+function switchState(block, arrow) {
+    
     cnt++;
-    textAbout.classList.toggle("about__storyFull")
+    console.log(cnt);
+    block.classList.toggle("about__storyFull")
     if (cnt % 2 === 0) {
-        textBot.textContent=""
-        textBot.innerHTML = "<p>Подробнее<br>▼</p>"
+        arrow.textContent = ""
+        arrow.innerHTML = "<p>Подробнее<br>▼</p>"
         console.log("Второй клик");
-    } else textBot.innerHTML = "<p>▲</p>"
-})
+    } else arrow.innerHTML = "<p>▲</p>"
+}
 
-
-textBot.addEventListener('click',()=> {
-    cnt++;
-    textAbout.classList.toggle("about__storyFull")
-    if (cnt % 2 === 0) {
-        textBot.textContent=""
-        textBot.innerHTML = "<p>Подробнее<br>▼</p>"
-        console.log("Второй клик");
-    } else textBot.innerHTML = "<p>▲</p>"
-})
+textBot.addEventListener('click', () => switchState(textAbout, textBot));
+textAbout.addEventListener('click', () => switchState(textAbout, textBot));
--- js/test.js
+++ js/test.js
@@ -1,7 +1,91 @@
+let response = await fetch("https://calendar-json-app.adaptable.app/fullyear/2024");
+let data = []
+if (response.ok) { // если HTTP-статус в диапазоне 200-299
+    // получаем тело ответа (см. про этот метод ниже)
+    data = await response.json();
 
-function daytosecond(days) {
-    return days*24*60*60
+} else {
+    alert("Ошибка HTTP: " + response.status);
+}
+console.log(data);
+
+
+let mounthArr = Object.keys(data)
+
+let firstElement = mounthArr.shift();
+
+
+let mounth = [];
+
+mounthArr.forEach(element => {
+    let arrFix = [].concat(...data[element])
+    let del = arrFix.shift()
+    if (arrFix[0] === 2) {
+        let newArr = []
+        arrFix.forEach(element => {
+            if (element != 0)
+
+                newArr.push(element - 1)
+        });
+        newArr.unshift(0,0,0,0,0,0)
+        newArr.push(newArr[newArr.length-1] + 1)
+        mounth.push({
+            name: element,
+            date: newArr
+        }
+        )
+    } else {
+        mounth.push({
+            name: element,
+            date: arrFix
+        }
+        )
+    }
+
+    console.log(arrFix);
+
+
+
+});
+
+
+
+
+
+let mounthList = document.querySelectorAll(".caledar__dayList")
+
+console.log(mounth);
+
+for (let i = 0; i < mounthList.length; i++) {
+
+    mounth[i].date.forEach(e => {
+        if (e > 0) {
+            let dateItem = document.createElement("li")
+            let dateLink = document.createElement('a')
+
+            dateItem.classList.add("calendar__day")
+            dateLink.classList.add("calendar__dayLink")
+            dateLink.append(e);
+            mounthList[i].append(dateItem)
+            dateItem.append(dateLink)
+        } else {
+            let dateItem = document.createElement("li")
+            let dateLink = document.createElement('a')
+
+            dateItem.classList.add("calendar__day")
+            dateLink.classList.add("calendar__dayLink")
+
+            mounthList[i].append(dateItem)
+            dateItem.append(dateLink)
+        }
+    });
 }
 
-console.log(daytosecond(1));
 
+
+
+
+// console.log(mounthList);
+// json["tour"].forEach(element => {
+//     console.log(element.date);
+// });
