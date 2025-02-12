2cdaeba5880ac47c8a4d9f1f3df4fae0c40ca04d
Astellono <94223615+Astellono@users.noreply.github.com>
2025-02-12 17:12:29 +0300

    new ex

--- js/regEx.js
+++ js/regEx.js
@@ -32,25 +32,6 @@ btnAuto.addEventListener('click', (e) => {
     
 
 
-    // // fio.setAttribute("disabled", true)
-    // // age.setAttribute("disabled", true)
-    // // tel.setAttribute("disabled", true)
-    // // city.setAttribute("disabled", true)
-    // // email.setAttribute("disabled", true)
-    // // rost.setAttribute("disabled", true)
-    // // ves.setAttribute("disabled", true)
-    // // staj.setAttribute("disabled", true)
-    // // fizNagr.setAttribute("disabled", true)
-    // // zabolevania.setAttribute("disabled", true)
-    // // davlenie.setAttribute("disabled", true)
-    // // opora.setAttribute("disabled", true)
-    // // perenosimost.setAttribute("disabled", true)
-    // // level.setAttribute("disabled", true)
-    // // prohod.setAttribute("disabled", true)
-
-    // // perenosimostGori.setAttribute("disabled", true)
-    // // ravn.setAttribute("disabled", true)
-
     btnAuto.style.background = 'gray'
     btnAuto.style.cursor = 'inherit'
     btnAuto.setAttribute("disabled", true)
